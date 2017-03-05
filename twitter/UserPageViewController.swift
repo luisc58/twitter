4299784d328e55254457b5fe6a17411dd481f84a
luis <luis.scholars@gmail.com>
2017-03-05 00:45:59 -0500

    MORE MORE

--- twitter/UserPageViewController.swift
+++ twitter/UserPageViewController.swift
@@ -2,7 +2,7 @@
 
 import UIKit
 
-class UserPageViewController: UIViewController {
+class UserPageViewController: UIViewController,UITabBarDelegate, UITableViewDataSource {
     //[[[[HEADER VIEW]]]]]
     @IBOutlet weak var profileImage: corners!
     @IBOutlet weak var name: UILabel!
@@ -22,16 +22,17 @@ class UserPageViewController: UIViewController {
         didSet 
         {
             User.userWithScreenName(tweets!.handle, success: { (user) in
-                self.userInfo = user
-                self.tweetsCount.text = "\(user.followersCount)"
-                self.header.setImageWith(user.bannerUrl!)
-                print("\(user.name)")
+                self.userInfo = user                                    //||||||||||||||||||||||||||||||||||||||||||||||||||||
+                self.tweetsCount.text = "\(user.followersCount)"        //|||[SETS HEADER PROPERTIES ACCORDING TO USERNAME]|||
+                self.header.setImageWith(user.bannerUrl!)               //||||||||||||||||||||||||||||||||||||||||||||||||||||
+                
             }, failure: { (error) in })}
         }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         setupHeader()
+        navItem()
         userScreenName = userScreenName != nil ? userScreenName : User.currentUser?.screenname
          
     }
@@ -42,9 +43,49 @@ class UserPageViewController: UIViewController {
         profileImage.setImageWith((tweets?.imgUrl)!)
         name.text = tweets?.username
         screenName.text = tweets?.handle
+    }
+    
+    //|||[NAVITEM]|||
+    private func navItem() {
         
+        let titleView = UIImageView(image: #imageLiteral(resourceName: "TwitterLogoBlue"))
+        titleView.frame = CGRect(x:0,y:0, width: 34, height: 34)
+        titleView.contentMode = .scaleAspectFit
+        
+        navigationItem.titleView = titleView
+    }
+    //||||||||||||||||||||||||
+    //|||[CELL PROPERTIES]||||
+    //||||||||||||||||||||||||||||||||||||||||||||
+    
+   
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {   //|||[RETURNS TWEETS ASSOCIATED WITH THAT USERNAME]|||
+        return 0
+    }
+    
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
     }
+    
+ 
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
     
 }
